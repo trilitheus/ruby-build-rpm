1d46903991a494c9ac844a2e97650902a6c84edd
Roy Crombleholme <roy.crombleholme@pam-it.com>
2017-09-15 09:42:01 +0100

    Generalise build script

---
+++ build_ruby.sh
@@ -0,0 +1,13 @@
+RUBY_VERSION='2.3.3'
+MAJOR_MINOR=$(echo $RUBY_VERSION | cut -f1,2 -d'.')
+
+cd /tmp
+wget https://cache.ruby-lang.org/pub/ruby/${MAJOR_MINOR}/ruby-${RUBY_VERSION}.tar.gz
+tar xzf ruby-${RUBY_VERSION}.tar.gz
+cd ruby-${RUBY_VERSION}
+./configure --prefix /usr/local --includedir=/usr/local/lib/ruby/include
+make
+make install DESTDIR=/tmp/installdir
+cd /tmp/destdir
+usr/local/bin/gem install bundle --no-ri --no-rdoc
+fpm -s dir -t rpm -n ruby -f -v ${RUBY_VERSION} -C /tmp/installdir -p ruby-VERSION_ARCH.rpm usr/local/bin usr/local/share/man usr/local/lib
